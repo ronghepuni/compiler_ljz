a38f3c3de21cd68a8ade0a17e0ba10f2b8a39866
XZQshiyu <2313392804@qq.com>
2025-04-27 11:28:08 +0000

    add todo in dce

--- src/passes/DeadCode.cpp
+++ src/passes/DeadCode.cpp
@@ -39,24 +39,6 @@ void DeadCode::mark(Function *func) {
     work_list.clear();
     marked.clear();
     // TODO: 标记无用变量
-    // 提示：
-    // 通过is_critical函数判断，将所有的无用变量标记为true
-
-    // for (auto &bb : func->get_basic_blocks()) {
-    //     for (auto &ins : bb.get_instructions()) {
-    //         if (is_critical(&ins)) {
-    //             marked[&ins] = true;
-    //             work_list.push_back(&ins);
-    //         }
-    //     }
-    // }
-
-    // while (work_list.empty() == false) {
-    //     auto now = work_list.front();
-    //     work_list.pop_front();
-
-    //     mark(now);
-    // }
 }
 
 void DeadCode::mark(Instruction *ins) {
@@ -82,24 +64,7 @@ bool DeadCode::sweep(Function *func) {
     // 4. 注意：删除指令时，需要先删除操作数的引用，然后再删除指令本身
     // 5. 删除指令时，需要注意指令的顺序，不能删除正在遍历的指令
     std::unordered_set<Instruction *> wait_del{};
-    // for (auto &bb : func->get_basic_blocks()) {
-    //     for (auto it = bb.get_instructions().begin();
-    //          it != bb.get_instructions().end();) {
-    //         if (marked[&*it]) {
-    //             ++it;
-    //             continue;
-    //         } else {
-    //             auto tmp = &*it;
-    //             wait_del.insert(tmp);
-    //             it++;
-    //         }
-    //     }
-    // }
-    // for (auto inst : wait_del)
-    //     inst->remove_all_operands();
-    // for (auto inst : wait_del)
-    //     inst->get_parent()->get_instructions().erase(inst);
-    // ins_count += wait_del.size();
+
     return not wait_del.empty(); // changed
 }
 
@@ -110,17 +75,6 @@ bool DeadCode::is_critical(Instruction *ins) {
     // 2. 如果是无用的分支指令，则无用
     // 3. 如果是无用的返回指令，则无用
     // 4. 如果是无用的存储指令，则无用
-    // if (ins->is_call()) {
-    //     auto call_inst = dynamic_cast<CallInst *>(ins);
-    //     auto callee = dynamic_cast<Function *>(call_inst->get_operand(0));
-    //     if (func_info->is_pure_function(callee))
-    //         return false;
-    //     return true;
-    // }
-    // if (ins->is_br() || ins->is_ret())
-    //     return true;
-    // if (ins->is_store())
-    //     return true;
     return false;
 }
 
